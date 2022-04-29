4142b72304a85197daa54b65845e1b8b29e23811
KongSungsik <ksjustin98@korea.ac.kr>
2022-04-29 16:42:05 +0900

    G2 newGameTwo Solved

---
+++ BaekJoon/G2_newGameTwo_17837.py
@@ -0,0 +1,212 @@
+# 새로운 게임2
+
+dr = [0,0,-1,1]
+dc = [1,-1,0,0]
+
+def MAP_printer(MAP):
+    for row in MAP:
+        print(row)
+    print()
+
+def input_getter():
+    N, K = map(int, input().split(' '))
+    MAP = []
+    for _ in range(N):
+        MAP.append(list(map(int, input().split(' '))))
+    agents_list = []
+    agentTowerMAP = [[False]*N for _ in range(N)] # 3차원 리스트는 우선 2차원을 만든 후 나중에 1차원 자료구조를 넣어주는 형태로 구현.
+    
+    for k in range(K):
+        """
+        agent = [agentNum, [agentPos], agentDir]
+
+        agent_list = [agent, agent, agent, ... ]
+
+        agentTower = [
+            ...
+            [ [agentNum, agentDir], [agentNum, agentDir], ... ] # 쌓여진 agent들의 순서
+            ...
+        ]
+        """
+        r, c, d = map(int, input().split(' '))
+        r -= 1
+        c -= 1
+        d -= 1
+        agent = [k, [r,c], d]
+        agents_list.append(agent) 
+
+        agentTowerMAP[r][c] = [agent] # 쌓을 수 있는 구조를 형성하려면?
+
+    for i in range(len(agentTowerMAP)):
+        for j in range(len(agentTowerMAP[0])):
+            if agentTowerMAP[i][j] == False:
+                agentTowerMAP[i][j] = list()
+
+
+
+    return N, K, MAP, agents_list, agentTowerMAP
+
+def reverse_agent_direction(direction):
+
+    if direction == 0:
+        direction = 1
+    elif direction ==1:
+        direction = 0
+    elif direction == 2:
+        direction = 3
+    elif direction == 3:
+        direction = 2
+
+    return direction
+
+def towerLengthChecker(agentTower):
+    if len(agentTower) >= 4:
+        return True
+    else:
+        return False
+
+
+def move_agents(agents_list, agentTowers, MAP, N, K):
+    endgame = False
+    for a in range(len(agents_list)):
+        agent = agents_list[a]
+        agentNum = agent[0]
+        noMovementFlag = False
+        # print(f"current agent : {agent}\n")
+
+        agentR, agentC = agent[1][0], agent[1][1]
+        agentDir = agent[2]
+        currentAgentTower = agentTowers[agentR][agentC]
+        # print(f"current agent Tower : {currentAgentTower}\n")
+        anyTowerLengthBiggerThanFourFlag = False
+        agentTowerIndex = currentAgentTower.index(agent) # 현재 agent위를 다 데리고 가기위한
+            
+        actualMovingAgentTower = currentAgentTower[agentTowerIndex:] # 현재 agent위들
+        # if currentAgentTower[:agentTowerIndex] == []:
+        #     agentTowers[agentR][agentC] = [currentAgentTower[:agentTowerIndex]] # 기존에 있던 Tower에서 움직이는 부분을 없애줌
+        # else:
+        agentTowers[agentR][agentC] = currentAgentTower[:agentTowerIndex]
+        # print(f"insert this to original towerPos {currentAgentTower[:agentTowerIndex]}")
+
+        tmp_r, tmp_c = agentR + dr[agentDir], agentC + dc[agentDir]
+        if 0 <= tmp_r < N and 0<= tmp_c < N:
+            mapColor = MAP[tmp_r][tmp_c]          
+            
+            if mapColor == 0:
+                agentTowers[tmp_r][tmp_c].extend(actualMovingAgentTower)
+                anyTowerLengthBiggerThanFourFlag = towerLengthChecker(agentTowers[tmp_r][tmp_c])
+
+            elif mapColor == 1:
+                actualMovingAgentTower.reverse()
+                # print(f"reversed tower : {actualMovingAgentTower}")
+
+                agentTowers[tmp_r][tmp_c].extend(actualMovingAgentTower)
+                anyTowerLengthBiggerThanFourFlag = towerLengthChecker(agentTowers[tmp_r][tmp_c])
+
+
+            elif mapColor == 2:
+                agents_list[a][2] = reverse_agent_direction(agentDir)
+                agentDir = agents_list[a][2]
+                tmp_r, tmp_c =  agentR + dr[agentDir], agentC + dc[agentDir]
+
+                if 0<= tmp_r < N and 0 <= tmp_c < N:
+                    newMapColor = MAP[tmp_r][tmp_c]
+                    if newMapColor == 0:
+                        agentTowers[tmp_r][tmp_c].extend(actualMovingAgentTower)
+                        anyTowerLengthBiggerThanFourFlag = towerLengthChecker(agentTowers[tmp_r][tmp_c])
+
+
+                    elif newMapColor == 1:
+                        actualMovingAgentTower.reverse()
+                        # print(f"reversed tower : {actualMovingAgentTower}")
+                        agentTowers[tmp_r][tmp_c].extend(actualMovingAgentTower)
+                        anyTowerLengthBiggerThanFourFlag = towerLengthChecker(agentTowers[tmp_r][tmp_c])
+
+
+                    else:
+                        agentTowers[agentR][agentC].extend(actualMovingAgentTower)
+                        noMovementFlag = True
+                        anyTowerLengthBiggerThanFourFlag = towerLengthChecker(agentTowers[tmp_r][tmp_c])
+                else:
+                    agentTowers[agentR][agentC].extend(actualMovingAgentTower)
+                    noMovementFlag = True
+                    # anyTowerLengthBiggerThanFourFlag = towerLengthChecker(agentTowers[tmp_r][tmp_c])
+                        
+        else:
+            agents_list[a][2] = reverse_agent_direction(agentDir)
+            agentDir = agents_list[a][2]
+            tmp_r, tmp_c =  agentR + dr[agentDir], agentC + dc[agentDir]
+
+            if 0<= tmp_r < N and 0 <= tmp_c < N:
+                newMapColor = MAP[tmp_r][tmp_c]
+                if newMapColor == 0:
+                    agentTowers[tmp_r][tmp_c].extend(actualMovingAgentTower)
+                    anyTowerLengthBiggerThanFourFlag = towerLengthChecker(agentTowers[tmp_r][tmp_c])
+
+
+                elif newMapColor == 1:
+                    actualMovingAgentTower.reverse()
+                    agentTowers[tmp_r][tmp_c].extend(actualMovingAgentTower)
+                    anyTowerLengthBiggerThanFourFlag = towerLengthChecker(agentTowers[tmp_r][tmp_c])
+
+
+                else:
+                    agentTowers[agentR][agentC].extend(actualMovingAgentTower)
+                    noMovementFlag = True
+
+                    anyTowerLengthBiggerThanFourFlag = towerLengthChecker(agentTowers[tmp_r][tmp_c])
+        if not noMovementFlag:
+            totalTowerPos = [tmp_r, tmp_c]
+
+            for agl in range(len(actualMovingAgentTower)):
+                actualMovingAgentTower[agl][1] = totalTowerPos
+                curAgent = actualMovingAgentTower[agl]
+                agentNumberIndex = curAgent[0]
+                # agentChangedDir = curAgent[2]
+
+                agents_list[agentNumberIndex][1] = totalTowerPos
+                # agents_list[agentNumberIndex][2] = agentChangedDir
+
+
+            agents_list[a][1] = [tmp_r, tmp_c]
+        #     print(f"updated agents_list : {agents_list}")
+        # print("after agentMove: ")
+        # MAP_printer(agentTowers)
+        
+        if anyTowerLengthBiggerThanFourFlag:
+            endgame = True
+            
+    return agents_list, agentTowers, endgame
+
+    
+
+        
+        
+
+
+if __name__ == "__main__":
+    N, K, MAP, agents_list, agentTowers = input_getter()
+    time_step = 0
+    timeOverflowFlag = False
+    # print("init agentTowers")
+    # MAP_printer(agentTowers)
+
+    # print(f"init agentList : {agents_list}")
+    while True:
+
+        agents_list, agentTowers, endgame = move_agents(agents_list, agentTowers, MAP, N, K)
+        time_step += 1
+        # print(f"current TIME STEP : {time_step}\n")
+
+        if endgame:
+            break
+        if time_step >= 1000:
+            timeOverflowFlag = True
+            break
+
+    if timeOverflowFlag:
+        print(-1)
+    else:
+        print(time_step)
+            
+
--- BaekJoon/_experiment_note.py
+++ BaekJoon/_experiment_note.py
@@ -1,93 +1,13 @@
-# connected components 찾기
-
-#1 MAP creation
-connected_components_map = [[0]*10 for _ in range(10)]
-for i in range(0,2):
-    connected_components_map[i][3:6] = [1]*(6-3)
-
-for i in range(3,7):
-    connected_components_map[i][7:] = [1]*(len(connected_components_map) - 7)
-
-for i in range(4, len(connected_components_map) -1):
-    connected_components_map[i][5] = 1
-
-for i in range(6,len(connected_components_map)):
-    connected_components_map[i][:3] = [1]*(3-0)
+threeD_map = [[[False]]*10 for _ in range(10)]
 
 def MAP_printer(MAP):
     for row in MAP:
         print(row)
     print()
 
-MAP_printer(connected_components_map)
+for r in range(len(threeD_map)):
+    for c in range(len(threeD_map[0])):
+        if not threeD_map[r][c][0]:
+            threeD_map[r][c][0] = list()
 
-#2 DFS로 찾기
-from collections import deque
-dr = [0,1,0,-1]
-dc = [1,0,-1,0]
-def find_connected_components(MAP):
-    """
-    connected components의 여러 기능들을 한번에 구현하는게 목표
-
-    1. 찾은 순서별로 넘버링한 MAP 가지고 오기
-    2. 찾은 component별 구성요소 인덱스 가지고 오기
-    3. component 개수 파악하기
-
-    -> 생각해보니 이 모든게 2를 하면 쉽게 구현되므로, 2만 하도록 하겠다.
-    """
-    R = len(MAP)
-    C = len(MAP[0])
-
-    
-    global_visited = [[False]*C for _ in range(R)]
-    global_components_indicies = []
-    for r in range(R):
-        for c in range(C):
-            if not global_visited[r][c]:
-                if MAP[r][c] != 0:
-                    this_init = [r, c]
-
-                    this_group_index = [this_init]
-
-                    queue = deque()
-                    queue.append(this_init)
-
-                    local_visited = [[False]*C for _ in range(R)]
-                    while queue:
-                        curData = queue.pop()
-                        curR, curC = curData[0], curData[1]
-                        global_visited[curR][curC] = True
-                        local_visited[curR][curC] = True
-
-                        for i in range(4):
-                            tmp_r, tmp_c = curR + dr[i], curC + dc[i]
-                            if 0<= tmp_r < R and 0 <= tmp_c < C:
-                                if local_visited[tmp_r][tmp_c] == False:
-                                    local_visited[tmp_r][tmp_c] = True
-                                    if MAP[tmp_r][tmp_c] == 1: # 조건 : 현재 위치와 인접하며, 값이 1인(존재하는) 칸
-                                        this_group_index.append([tmp_r, tmp_c])
-                                        global_visited[tmp_r][tmp_c] = True
-                                        queue.append([tmp_r, tmp_c])
-                    MAP_printer(local_visited)
-                    global_components_indicies.append(this_group_index)
-                        
-    return global_components_indicies
-                    
-
-                        
-def numbering_connected_components(components_indicies, MAP):
-    numberedMAP = MAP[:]
-    component_numbering = 0
-    for indicies in components_indicies:
-        component_numbering += 1
-        for index in indicies:
-            r, c = index[0], index[1]
-            numberedMAP[r][c] = component_numbering
-    return numberedMAP
-
-
-if __name__ == '__main__':
-    components_indicies = find_connected_components(connected_components_map)
-    numberedMAP = numbering_connected_components(components_indicies, connected_components_map)
-
-    MAP_printer(numberedMAP)
+MAP_printer(threeD_map)
